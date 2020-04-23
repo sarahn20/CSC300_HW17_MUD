eb0505b35d4f189a02afb9157900c4ca075cd632
sarahn20 <56127151+sarahn20@users.noreply.github.com>
2020-04-23 08:07:53 -0500

    Assignment18Commit

--- Room.cpp
+++ Room.cpp
@@ -1,154 +1,125 @@
 #include "Room.hpp"
+#include "Door.hpp"
+#include "Student.hpp"
+#include "LinkedListOfStudents.hpp"
 #include <iostream>
 
-Room::Room(string roomName)
+Room::Room(string title)
 {
-    this->roomName = roomName;
-    this->northDoor = NULL;
-    this->southDoor= NULL;
-    this->eastDoor = NULL;
-    this->westDoor = NULL;
+    this->title = title;
+    this->currentNumberOfDoors = 0;
+    this->collectionOfDoors[10];
+    
 }
 
-void Room::setNorthDoor(Door* givenDoor)
+void Room::constructMap()
 {
-    this->northDoor = givenDoor;
+    
+
+    Student* player2 = new Student("Mr. Gonzales");
+   
+    this->ll->addFront(player1);
+    this->ll->addFront(player2);
+    Room* hallway1_5 = new Room("Hallway 1.5"); //2
+    Room* hallway1 = new Room("Hallway 1"); //3
+    Room* hallway0 = new Room("Hallway 0"); //4
+    Room* classroom120 = new Room("Classroom 120"); //5
+    Room* lobby = new Room("Lobby"); //6
+    Room* esports = new Room("Esports"); //7
+    Room* locklairsOffice = new Room("Locklair's Office"); //8
+    Room* serverRoom = new Room("Server Room"); //9
+    Room* hallway2 = new Room("Hallway 2"); //10
+    Room* classroom118 = new Room("Classroom 118"); //11
+    Room* macLab2 = new Room("Mac Lab 2"); //12
+    Room* macLab1 = new Room("Mac Lab 1"); //13
+    Room* advLab1 = new Room("Advance Lab 2"); //14
+    Room* advLab2 = new Room("Advance Lab 1"); //15
+    player2->setCurrentRoom(hallway1);
+    Door* hw1_75ANDhw1_5 = new Door("East",this, "West", hallway1_5); //1
+    Door* hw1_5ANDhw1 = new Door("North",hallway1_5,"South", hallway1); //2
+    Door* hw1ANDhw0 = new Door("North", hallway1, "South", hallway0); //3
+    Door* hw0ANDclassroom120 = new Door("North", hallway0, "South", classroom 120); //4
+    Door* hw0ANDlobby = new Door("East", hallway0, "Wester", lobby); //5
+    Door* lobbyANDesports = new Door("North", lobby, "South", esports); //6
+    Door* lobbyANDlocklairsOffice = new Door("East", lobby, "West", locklairsOffice); //7
+    Door* lobbyANdserverRoom = new Door("South", lobby, "North", serverRoom); //8
+    Door* hw1_5ANDhw2 = new Door("North", hallway2, "South", hallway1_5); //9
+    Door* hw2ANDmacLab1 = new Door("North", macLab1, "South", hallway2); //10
+    Door* hw2ANDadvLab1 = new Door("East", advLab1, "West",hallway2); //11
+    Door* advLab1ANDadvLab2 = new Door("North", advLab2, "South", advLab1); //12
+    Door* macLab1ANDmacLab2 = new Door("West", macLab2, "East", macLab1); //13
+    Door* macLab2ANDclassroom118 = new Door("West", macLab2, "South", classroom118); //14
+    Door* hw2ANDclassroom118 = new Door("West", classroom118, "East", hallway2)
+    }
+
+string Room::getTitle()
+{
+    return this->title;
 }
 
-void Room::setSouthDoor(Door* givenDoor)
+void Room::onRoomEnter()
 {
-    this->southDoor = givenDoor;
-}
-
-void Room::setEastDoor(Door* givenDoor)
-{
-    this->eastDoor = givenDoor;
-}
-
-void Room::setWestDoor(Door* givenDoor)
-{
-    this->westDoor = givenDoor;
-}
-
-string Room::getRoomName()
-{
-    return this->roomName;
-}
-
-void Room::runMud()
-{
-    this->inhabitants->addFront("Sarah");
-    cout << "You are in room: " << this->getRoomName() << "\n";
-    cout << "Room inhabitants include: " << this->inhabitants->display() << "\n";
-    if(this->northDoor)
+    player1->setCurrentRoom(this);
+    cout << "You are in room: " << this->title << "\n";
+    this->inThisRoom();
+    this->collectionOfDoors;
+    for(int i = 0; i < 10; i++)
     {
-        if(this != this->northDoor->room1)
+        if(this->collectionOfDoors[i]->getRoomA() == this)
         {
-            cout << "The North Door leads to: " << this->northDoor->room1->getRoomName();
-        }
-        else if(this != this->northDoor->room2)
+            cout << "The " << this->collectionOfDoors[i]->getDirectionToRoomB() << " leads to " << this->collectionOfDoors[i]->getRoomB()->getTitle() << "\n";
+        } 
+        else if(this->collectionOfDoors[i]->getRoomB() == this)
         {
-           cout << "The North Door leads to: " << this->northDoor->room1->getRoomName(); 
+            cout << "The " << this->collectionOfDoors[i]->getDirectionToRoomA() << " leads to " << this->collectionOfDoors[i]->getRoomA()->getTitle() << "\n";
+        } 
+    }
+    string direction;
+    cout << "Enter the direction you would like to go: ";
+    cin >> direction;
+    for(int i = 0; i < 10; i++)
+    {
+        if(this->collectionOfDoors[i]->getRoomA() != this)
+        {
+            if(this->collectionOfDoors[i]->getDirectionToRoomA() == direction)
+            {
+                this->collectionOfDoors[i]->getRoomA()->onRoomEnter();
+            }
+            
+        } 
+        else if(this->collectionOfDoors[i]->getRoomB() != this)
+        {
+            if(this->collectionOfDoors[i]->getDirectionToRoomB() == direction)
+            {
+                this->collectionOfDoors[i]->getRoomB()->onRoomEnter();
+            }
+            
+        } 
+    }
+
+
+}
+
+void Room::inThisRoom()
+{
+    StudentNode* currNode = ll->getHead();
+    cout << "These are the students in the room: ";
+    while(int i = 0 < ll->getCount())
+    {
+        if(currNode->getPayload()->getCurrentRoom() == this)
+        {
+            cout << currNode->getPayload()->getName() << ", ";
+            
         }
+        currNode = currNode->getNextNode();
+        i++;
         
-    }
-    if(this->southDoor)
-    {
-        if(this != this->southDoor->room1)
-        {
-            cout << "The South Door leads to: " << this->southDoor->room1->getRoomName();
-        }
-        else if(this != this->southDoor->room2)
-        {
-           cout << "The South Door leads to: " << this->southDoor->room1->getRoomName(); 
-        }
-        
-    }
-    if(this->eastDoor)
-    {
-        if(this != this->eastDoor->room1)
-        {
-            cout << "The East Door leads to: " << this->eastDoor->room1->getRoomName();
-        }
-        else if(this != this->eastDoor->room2)
-        {
-           cout << "The East Door leads to: " << this->eastDoor->room1->getRoomName(); 
-        }
-        
-    }
-    if(this->westDoor)
-    {
-        if(this != this->westDoor->room1)
-        {
-            cout << "The West Door leads to: " << this->westDoor->room1->getRoomName();
-        }
-        else if(this != this->westDoor->room2)
-        {
-           cout << "The West Door leads to: " << this->westDoor->room1->getRoomName(); 
-        }
-    }
-    cout << "Which door would you like to go through?";
-    string goalDoor;
-    cin >> goalDoor;
-    this->inhabitants->removeFront();
-    if(goalDoor == "north door")
-    {
-        if(this != this->northDoor->room1)
-        {
-            cuwCS->setCurrRoom(this->northDoor->room1);
-            this->runMud();
-            
-        }
-        else if(this != this->northDoor->room2)
-        {
-           cuwCS->setCurrRoom(this->northDoor->room2);
-           this->runMud(); 
-        }
 
     }
-    else if(goalDoor == "south door")
-    {
-        if(this != this->southDoor->room1)
-        {
-            cuwCS->setCurrRoom(this->southDoor->room1);
-            this->runMud();
-            
-        }
-        else if(this != this->southDoor->room2)
-        {
-           cuwCS->setCurrRoom(this->southDoor->room2);
-           this->runMud(); 
-        }
+   
 
-    }
-    else if(goalDoor == "east door")
-    {
-        if(this != this->eastDoor->room1)
-        {
-            cuwCS->setCurrRoom(this->eastDoor->room1);
-            this->runMud();
-            
-        }
-        else if(this != this->eastDoor->room2)
-        {
-           cuwCS->setCurrRoom(this->eastDoor->room2);
-           this->runMud(); 
-        }
 
-    }
-    else if(goalDoor == "west door")
-    {
-        if(this != this->westDoor->room1)
-        {
-            cuwCS->setCurrRoom(this->westDoor->room1);
-            this->runMud();
-            
-        }
-        else if(this != this->westDoor->room2)
-        {
-           cuwCS->setCurrRoom(this->westDoor->room2);
-           this->runMud(); 
-        }
 
-    }
+
 }
+
