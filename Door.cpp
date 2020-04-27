e75f6a0bee2203ff5aae997fc687a92aefc52356
sarahn20 <56127151+sarahn20@users.noreply.github.com>
2020-04-27 18:55:08 -0500

    4-27-2020 Commit

--- Door.cpp
+++ Door.cpp
@@ -7,40 +7,13 @@ Door::Door(string directionToRoomA, Room* roomA, string directionToRoomB, Room* 
     this->roomA = roomA;
     this->roomB = roomB;
 
-    roomA->currentNumberOfDoors++;
-    int index = 0;
-    while(index < 10) 
-    {
-        if(roomA->collectionOfDoors[index] == NULL)
-        {
-            roomA->collectionOfDoors[index] = this;
-            break;
-        }
-        else
-        {
-            index++;
-        }
-    }
-    roomB->currentNumberOfDoors++;
-    int indexB = 0;
-    while(indexB < 10) 
-    {
-        if(roomA->collectionOfDoors[indexB] == NULL)
-        {
-            roomA->collectionOfDoors[indexB] = this;
-            break;
-        }
-        else
-        {
-            indexB++;
-        }
-    }
+    roomA->addDoor(this);
+    roomB->addDoor(this);
 }
 
 Room* Door::getRoomA()
 {
     return this->roomA;
-    
 }
 
 Room* Door::getRoomB()
