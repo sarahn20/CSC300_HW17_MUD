bcc84c87fae0385b2e5e188c3813a300da419df6
sarahn20 <56127151+sarahn20@users.noreply.github.com>
2020-05-07 02:20:15 -0500

    5-7-2020 Commit

--- Room.cpp
+++ Room.cpp
@@ -1,101 +1,134 @@
 #include "Room.hpp"
-#include "Door.hpp"
-#include "Student.hpp"
-#include "LinkedListOfStudents.hpp"
 #include <iostream>
+#include "Door.hpp"
+
+using namespace std;
 
 Room::Room(string title)
 {
     this->title = title;
     this->currentNumberOfDoors = 0;
-    this->collectionOfDoors[10];
+    this->maxItemCount = 10;
+    this->currentItemCount = 0;
+    this->shelf = (Item**)malloc(this->maxItemCount * sizeof(Item*));
+
+}
+
+void Room::displayShelfContents()
+{
+    for(int i = 0; i < this->currentItemCount; i++)
+    {
+        cout << this->shelf[i]->getName() << "\n";
+    }
+}
+
+bool Room::addItem(Item* anItem)
+{
+    if(this->currentItemCount == this->maxItemCount)
+    {
+        return false;
+    }
+    else
+    {
+        this->shelf[this->currentItemCount] = anItem;
+        this->currentItemCount++;
+        return true;
+    } 
+}
+
+Item* Room::removeItem(string anItem)
+{
+    Item* tempItem = 0;
+    for(int i = 0; i < this->currentItemCount; i++)
+    {
+        if(this->shelf[i]->getName() == anItem)
+        {
+            tempItem = this->shelf[i];
+            while(i < this->currentItemCount-1)
+            {
+                this->shelf[i] = this->shelf[i+1];
+                i++;
+            }
+            
+        }
+    }
+    if(tempItem != 0)
+    {
+        this->currentItemCount--;
+        
+    }
+    return tempItem;
+}
+
+void Room::addDoor(Door* aDoor)
+{
+    this->collectionOfDoors[this->currentNumberOfDoors] = aDoor;
+    this->currentNumberOfDoors++;
+    this->theStudents = new LinkedListOfStudents();
+}
+
+void Room::addStudent(Student* aStudent)
+{
+    if(this->theStudents->indexOf(aStudent) == -1)
+    {
+        this->theStudents->addFront(aStudent);
+        aStudent->setCurrentRoom(this);
+    } 
+}
+
+void Room::removeStudent(Student* aStudent)
+{
+    int indexOfStudent = this->theStudents->indexOf(aStudent);
+    if(indexOfStudent != -1)
+    {
+        this->theStudents->removeAtIndex(indexOfStudent);
+        aStudent->setCurrentRoom(0); //the student is in limbo
+    }
+}
+
+void Room::takeDoor(Student* aStudent, string direction)
+{
+    Door* tempDoor;
+    Room* currentRoom = this;
+    for(int i = 0; i < this->currentNumberOfDoors; i++)
+    {
+        tempDoor = this->collectionOfDoors[i];
+        if(tempDoor->getDirectionToOtherRoom(currentRoom) == direction)
+        {
+            Room* theOtherRoom = tempDoor->getTheOtherRoom(currentRoom);
+            
+            //remove the student from the current room
+            this->removeStudent(aStudent);
+
+            //put the student in the new room
+            theOtherRoom->addStudent(aStudent);
+        }
+    }
+}
+
+void Room::display()
+{
+    cout << this->title << "\n";
+
+    //show the potential doors
+    cout << "Obvious Exits: ";
+    Door* tempDoor = 0;
     
-}
-
-void Room::addDoor(Door* d)
-{
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < this->currentNumberOfDoors; i++)
     {
-        if(this->collectionOfDoors[i] == 0)
-        {
-            this->collectionOfDoors[i] = d;
-            this->currentNumberOfDoors++;
-            break;
-        }
+        tempDoor = this->collectionOfDoors[i];
+        cout << tempDoor->getDirectionToOtherRoom(this) << " ";
     }
-}
+    cout << "\n Also Here: ";
+    Student* tempStudent;
 
-string Room::getTitle()
-{
-    return this->title;
-}
-
-void Room::startGame(Student* player, LinkedListOfStudents* ls)
-{
-    player->setCurrentRoom(this);
-    cout << "YOU ARE IN: " << this->getTitle() << "\n";
-    cout << "Students in " << this->getTitle() << " include: ";
-    StudentNode* currNode = ls->getHead();
-    for(int i = 0; i < ls->getCount(); i++)
+    for(int i = 0; i < this->theStudents->getCount(); i++)
     {
-        if(currNode->getPayload()->getCurrentRoom() == this)
-        {
-            if(currNode != ls->getHead())
-            {
-                cout << ", ";
-            }
-            cout << currNode->getPayload()->getName();
-            
-        }
-        currNode = currNode->getNextNode();
+        tempStudent = this->theStudents->getAtIndex(i);
+        cout << tempStudent->getName() << " ";
     }
+    cout << "\n Items in Room: ";
+    this->displayShelfContents();
+
     cout << "\n";
-    for(int i = 0; i < 10; i++) // displays all possible rooms to go to
-    {
-        if(this->collectionOfDoors[i] != 0)
-        {
-            if(this->collectionOfDoors[i]->getRoomA() == this)
-            {
-                cout << this->collectionOfDoors[i]->getRoomB()->getTitle() << " is through the " << this->collectionOfDoors[i]->getDirectionToRoomB() << " door" << "\n";
-            }
-            else if(this->collectionOfDoors[i]->getRoomB() == this)
-            {
-                cout << this->collectionOfDoors[i]->getRoomA()->getTitle() << " is through the " << this->collectionOfDoors[i]->getDirectionToRoomA() << " door" << "\n";
-            }
-
-        }
-    }
-    string userInput = "";
-    cout << "Enter the direction where you would like to go next: ";
-    cin >> userInput;
-    cout << "\n";
-    for(int j = 0; j < 10; j++)
-    {
-        for(int i = 0; i < 10; i++) //sends user to their seleted room
-        {
-            if(this->collectionOfDoors[i] != 0)
-            {
-                if(this->collectionOfDoors[i]->getRoomA() == this)
-                {
-                    if(userInput == this->collectionOfDoors[i]->getDirectionToRoomB())
-                    {
-                        this->collectionOfDoors[i]->getRoomB()->startGame(player, ls);
-                    }
-                
-                }
-                else if(this->collectionOfDoors[i]->getRoomB() == this)
-                {
-                    if(userInput == this->collectionOfDoors[i]->getDirectionToRoomA())
-                    {
-                        this->collectionOfDoors[i]->getRoomA()->startGame(player, ls);
-                    }
-                    
-                }
-
-            }
-            
-        }
-    }
-    cout << "*There's no door in that direction! Try again.*" << "\n";
-    this->startGame(player,ls);
 }
