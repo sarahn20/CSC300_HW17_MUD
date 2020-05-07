f2fafc1ab0cfd25ac829b8d2ecc9d7e8b9540d91
sarahn20 <56127151+sarahn20@users.noreply.github.com>
2020-05-07 02:17:22 -0500

    5-7-2020 Commit

--- Room.hpp
+++ Room.hpp
@@ -2,10 +2,10 @@
 #define Room_hpp
 
 #include <string>
+#include "LinkedListOfStudents.hpp"
+#include "Item.hpp"
 
 class Door; // allows us to reference door when door refereneces Room.
-class Student;
-class LinkedListOfStudents;
 
 using namespace std;
 
@@ -15,12 +15,23 @@ class Room
         string title;
         Door* collectionOfDoors[10];
         int currentNumberOfDoors;
-        
+        LinkedListOfStudents* theStudents;
+        int maxItemCount;
+        int currentItemCount;
+        Item** shelf;
+
     
     public:
         Room(string title);
-        void addDoor(Door* d);
-        string getTitle();
-        void startGame(Student* player, LinkedListOfStudents* ls);
+        void addDoor(Door* aDoor);
+        Item* removeItem(string anItem);
+        void display();
+        void addStudent(Student* aStudent);
+        void removeStudent(Student* aStudent);
+        void takeDoor(Student* aStudent, string direction);
+        void displayShelfContents();
+        bool addItem(Item* anItem);
+
+
 };
 #endif
